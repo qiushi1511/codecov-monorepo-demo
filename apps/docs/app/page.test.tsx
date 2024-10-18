a3ad475f46c40387197d6c8b4508605a0f4be36b
alex <qiushi1511@gmail.com>
2024-10-18 17:22:04 +0800

    Setup Jest for apps/docs

---
+++ apps/docs/app/page.test.tsx
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import Page from "./page";
+
+describe("Page", () => {
+  it("renders a heading", () => {
+    render(<Page />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
