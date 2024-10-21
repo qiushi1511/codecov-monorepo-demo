ae26c5cf4d4620ff40fa3ff89f86273cc3f7e656
alex <qiushi1511@gmail.com>
2024-10-21 18:10:05 +0800

    Use istanbul instead

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    coverage: {
+      ...configDefaults.coverage,
+      provider: "istanbul",
+      exclude: [
+        ...configDefaults.coverage.exclude!,
+        "next.config.mjs",
+        "app/layout.tsx",
+      ],
+    },
   },
 });
